84408288854b5185bd25adb1ecc51782fa169b74
shuyang <shuyang.chou@hngf.com.tw>
2020-07-23 16:47:15 +0800

    Add comments for config-overrides

--- config-overrides.js
+++ config-overrides.js
@@ -2,9 +2,17 @@
  * @name Override Webpack Aliases
  * @description 設定React專案的alias路徑
  * @example
+ * 1) 設定Alias
  * 'alias': path.resolve(__dirname, `${paths.appSrc}/`), 路徑從src開始
  * Ex: '@assets': path.resolve(__dirname, `${paths.appSrc}/assets/`),
+ * 2) 修改package.json中的script
+ * script: {
+    "start": "react-app-rewired start",
+    "build": "react-app-rewired build",
+    "test": "react-app-rewired test",
+  }
  * @see https://github.com/aze3ma/react-app-rewire-aliases
+ * @see https://jaketrent.com/post/change-webpack-config-create-react-app-without-ejecting/
  */
 const rewireAliases = require('react-app-rewire-aliases')
 const { paths } = require('react-app-rewired')
