6ed12835e456dae1a3c7e8ceaae9f84fe339e153
shuyang <shuyang.chou@hngf.com.tw>
2020-08-12 17:37:59 +0800

    Add redux and redux-thunk

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,30 @@
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import App from './App'
-
 import '@assets/styles/reset.css'
+
+import {
+  increment,
+  decrement,
+  asyncDecrement,
+} from '@/actions/countActions'
+
+import App from '@/App'
+import store from '@/store'
+
+store.dispatch(increment())
+store.dispatch(increment())
+store.dispatch(decrement())
+store.dispatch(asyncDecrement())
 
 ReactDOM.render(
   // forceRefresh可以強迫在每次路由改變時重新加載頁面，解決每個頁面要scrollTop的問題，可以試試看
-  <BrowserRouter forceRefresh={true}>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter forceRefresh={true}>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 )
